c7f670741917337e9c4744608d1f9735a4918f6f
Lasantha Mudalige <lasantha1130@gmail.com>
2023-11-21 11:28:45 +0530

    add a comment

--- debian/install.sh
+++ debian/install.sh
@@ -12,6 +12,7 @@ deb-src http://deb.debian.org/debian-security/ bookworm-security main contrib no
 deb http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
 deb-src http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware" > /etc/apt/sources.list
 
+# Add backport to the end of the same file
 # Enable bookworm backports
 printf "\n# bookworm-backports
 deb http://deb.debian.org/debian bookworm-backports main contrib non-free
