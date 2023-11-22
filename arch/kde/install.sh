a28f74d6d387b0d6b59504f6fe972a8ebc478364
lasantha <lasantha1130@gmail.com>
2023-11-22 23:06:14 +0530

    change apt command to pacman

--- arch/kde/install.sh
+++ arch/kde/install.sh
@@ -7,7 +7,7 @@ sudo systemctl start bluetooth.service
 sudo systemctl enable bluetooth.service
 
 # Install tlp
-# sudo apt install tlp tlp-rdw -y
+# sudo pacman -S tlp tlp-rdw
 
 # Enable the firewall
 sudo ufw enable
