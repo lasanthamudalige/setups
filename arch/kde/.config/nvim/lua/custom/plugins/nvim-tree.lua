ee30b6cf502c08bed83b801d1cf9aac21f3de32c
lasantha <lasantha1130@gmail.com>
2023-12-05 00:41:08 +0530

    add neovim config

--- arch/kde/.config/nvim/lua/custom/plugins/nvim-tree.lua
+++ arch/kde/.config/nvim/lua/custom/plugins/nvim-tree.lua
@@ -1,61 +1,61 @@
 return {
   "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   config = function()
-    local nvimtree = require("nvim-tree")
+    require("nvim-tree").setup {}
+    
+  -- disable netrw at the very start of your init.lua
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
 
-    -- recommended settings from nvim-tree documentation
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+  -- set termguicolors to enable highlight groups
+  vim.opt.termguicolors = true
 
-    -- change color for arrows in tree to light blue
-    vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
-    vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
+  -- empty setup using defaults
+  require("nvim-tree").setup()
 
-    -- configure nvim-tree
-    nvimtree.setup({
-      view = {
-        width = 35,
-        relativenumber = true,
-      },
-      -- change folder arrow icons
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
-            },
-          },
-        },
-      },
-      -- disable window_picker for
-      -- explorer to work well with
-      -- window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      filters = {
-        custom = { ".DS_Store" },
-      },
-      git = {
-        ignore = false,
-      },
-    })
+  -- OR setup with some options
+  require("nvim-tree").setup({
+   sort = {
+     sorter = "case_sensitive",
+   },
+   view = {
+     width = 30,
+   },
+   renderer = {
+    group_empty = true,
+   },
+   filters = {
+     dotfiles = true,
+   },
+     })
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+  local function my_on_attach(bufnr)
+    local api = require "nvim-tree.api"
 
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+   local function opts(desc)
+     return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+   end
+
+   -- default mappings
+   api.config.mappings.default_on_attach(bufnr)
+
+   -- custom mappings
+   vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+   vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+  end
+
+  -- pass to setup along with your other options
+  require("nvim-tree").setup {
+  ---
+  on_attach = my_on_attach,
+  ---
+  }
   end,
 }
+
+
