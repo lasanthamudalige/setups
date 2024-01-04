c97f4fd894ca888ee1a53269237918fab8414e23
lasantha mudalige <lasantha1130@gmail.com>
2024-01-04 15:58:58 +0530

    remove ssh key generate and oh my zsh instakk and zsh auto complete

--- arch/kde/install.sh
+++ arch/kde/install.sh
@@ -27,7 +27,6 @@ cd Programs
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-cd ..
 
 # Install nvm
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
@@ -61,16 +60,6 @@ fc-cache -vf
 # Enable mongodb
 #sudo systemctl enable mongodb
 
-# Generate github ssh key
-./generate_new_github_ssh_key.sh
-
 # Add custom commands to .bashrc file
 cd ~
 printf "\n# Custom commands\n# command to wish facebook birthdays\nalias wish='micromamba run -n base ~/Development/shell-scripts/fb_birthday_wisher.sh'" >>.bashrc
-
-# Install oh my zsh
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-sh install.sh
-
-# Add zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
