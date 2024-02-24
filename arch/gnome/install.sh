5a1316d21327a6032ee0129e296f201e5821f66c
lasantha mudalige <lasantha1130@gmail.com>
2024-02-24 15:12:06 +0530

    remove unuseful symbols

--- arch/gnome/install.sh
+++ arch/gnome/install.sh
@@ -31,7 +31,7 @@ makepkg -si
 # Install nvm
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
-# ~~Install miniconda (use micromamba)~~
+# Install miniconda (use micromamba)
 #mkdir -p ~/miniconda3
 #wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 #bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
