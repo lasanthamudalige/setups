91bfd29804773c5b6a769743d84d8df5af421f87
Lasantha Mudalige <lasantha1130@gmail.com>
2024-06-18 02:51:28 +0530

    add base emacs config

---
+++ arch/kde/emacs/elpa/evil-20240616.1645/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20240616.1645" "Extensible vi layer"
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "7bfdb135882495ab7528b87c121ddafc3640d60e" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulations")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
