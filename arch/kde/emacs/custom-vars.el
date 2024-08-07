e98ef766c4d480ab519a420ea94adc89470417c3
Lasantha Mudalige <lasantha1130@gmail.com>
2024-08-07 22:58:25 +0530

    update emacs config

--- arch/kde/emacs/custom-vars.el
+++ arch/kde/emacs/custom-vars.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company eglot python-mode consult-themes @ treesit-auto undo-tree dap-mode lsp-treemacs lsp-ivy helm-lsp lsp-ui lsp-mode rainbow-mode ws-butler retrie dashboard-ls vterm dashboard which-key rainbow-delimiters diminish consult nerd-icons-completion marginalia vertico orderless cape nerd-icons-corfu corfu diff-hl magit nerd-icons-ibuffer nerd-icons-dired eat org-superstar toc-org yasnippet-snippets projectile doom-modeline gruvbox-theme general evil-collection evil)))
+   '(flycheck company eglot python-mode consult-themes @ treesit-auto undo-tree dap-mode lsp-treemacs lsp-ivy helm-lsp lsp-ui lsp-mode rainbow-mode ws-butler retrie dashboard-ls vterm dashboard which-key rainbow-delimiters diminish consult nerd-icons-completion marginalia vertico orderless cape nerd-icons-corfu corfu diff-hl magit nerd-icons-ibuffer nerd-icons-dired eat org-superstar toc-org yasnippet-snippets projectile doom-modeline gruvbox-theme general evil-collection evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
