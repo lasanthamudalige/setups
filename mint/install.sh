98a607d46d6310fab3e8ee303ac809ba899953cc
Lasantha Mudalige <lasantha1130@gmail.com>
2024-10-26 18:50:40 +0530

    remove zsh duplicate

--- mint/install.sh
+++ mint/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install tlp
-sudo apt install tlp tlp-rdw -y
+#sudo apt install tlp tlp-rdw -y
 
 # Install deb-get with dependencies
 sudo apt install curl lsb-release wget -y
@@ -14,4 +14,10 @@ sudo apt install fonts-jetbrains-mono fonts-firacode -y
 fc-cache -fv
 
 # Install basic set of applications
-sudo apt install ranger kitty neofetch gimp chromium obs-studio neovim zsh fwupd python3 python3-tk python3-pip python3-pylsp sqlite3 emacs cmake libtool ripgrep obsidian solaar vlc audacious ubuntu-restricted-extras htop btop nodejs npm zsh redhsift -y
+sudo apt install ranger kitty neofetch gimp chromium obs-studio neovim zsh fwupd python3 python3-tk python3-pip python3-pylsp sqlite3 emacs cmake libtool ripgrep solaar vlc audacious ubuntu-restricted-extras htop btop nodejs npm redshift -y
+
+# Auto remove unused programs
+#sudo apt remove rhythmbox celluloid -y
+
+# Auto remove unused packages
+sudo apt autoremove -y
